2e9e533d829e2559b1bba0a7266b553d82b62849
Yishuo Zhang 20243007047 <yishuozhang941@gmail.com>
2026-03-30 20:01:17 +0800

    Implement start_read logic for Monitor

--- readers_writers.py
+++ readers_writers.py
@@ -55,7 +55,10 @@ class ReadersWritersMonitor:
         """
         with self.condition:
             # TODO: Replace 'pass' with your logic
-            pass
+            while self.active_writers > 0:
+                self.condition.wait()
+            self.active_readers += 1
+            print(f"Reader {reader_id} starts reading. Active readers = {self.active_readers}")
 
     def end_read(self, reader_id: int) -> None:
         """
